44a3c6151a906935073b4a284717712abd46cf57
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-12-11 08:28:06 -0800

    Adding Middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,15 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // Allow the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+
 const server = createServer(app);
 
 server.listen(3000);
