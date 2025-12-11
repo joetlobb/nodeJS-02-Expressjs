c7f4193c4670c90b42d6fb0a46b9ec1730732a6c
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-12-11 09:04:35 -0800

    Parsing Incoming Requests

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Add Product Page</h1>");
-  });
+app.use(bodyParser.urlencoded());
 
-app.use('/', (req, res, next) => {
-  console.log("In another middleware");
+// Routes handling middlewares
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express</h1>");
 });
 
