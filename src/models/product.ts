042e898ea0deb7086c854f3aa042e7a6da608253
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-12-19 23:16:12 -0800

    Defining a Model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,38 +1,29 @@
-import db from '../utils/database.ts';
-import type { IProduct } from "../types/products.ts";
+import { DataTypes } from "sequelize";
+import sequelize from "../utils/database.ts";
 
-export const products: IProduct[] = [];
-
-export class Product {
-    private title: string;
-    private imageUrl: string;
-    private price: number;
-    private description: string;
-    private id: string;
-
-    constructor(title: string, imageUrl: string, price: number, description: string, id?: string) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.description = description;
-        this.id = id ? id : Math.random().toString();
+const Product = sequelize.define("product", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
     }
+}); 
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, description, url) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.description, this.imageUrl]
-        );
-    }
-
-    static deleteById(id: string): void {
-
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products')
-    }
-
-    static findById(id: string) {
-        return db.execute<IProduct[]>('SELECT * FROM products WHERE products.id = ?', [id])
-    }
-}
+export default Product;
