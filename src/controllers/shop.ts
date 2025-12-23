7e34ce5cf0bd15add190fb937991bc7ea4aaf166
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-12-23 14:05:49 -0800

    Fetching All and a Single Product

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -1,34 +1,33 @@
-// import Product from "../models/product.ts";
-// import type { IRequestHandler } from "../types/requestHandler.ts";
-// import type { IProduct } from "../types/products.ts";
+import Product from "../models/product.ts";
+import type { IRequestHandler } from "../types/requestHandler.ts";
 
-// export const getProducts: IRequestHandler = (req, res, next) => {
-//   Product.fetchAll()
-//     .then((products) => {
-//       res.render("shop/product-list", {
-//         prods: products,
-//         pageTitle: "All Products",
-//         path: "/products",
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+export const getProducts: IRequestHandler = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-// export const getIndex: IRequestHandler = (req, res, next) => {
-//   Product.fetchAll()
-//     .then((products) => {
-//       res.render("shop/index", {
-//         prods: products,
-//         pageTitle: "Shop",
-//         path: "/",
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+export const getIndex: IRequestHandler = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 // export const getCart: IRequestHandler = (req, res, next) => {
 //   const user = req.user;
@@ -87,26 +86,25 @@
 //   }
 // };
 
-// export const getProduct: IRequestHandler = (req, res, next) => {
-//   const prodId = req.params.productId;
-//   if (prodId) {
-//     Product.findById(prodId)
-//       .then((product) => {
-//         if (product) {
-//           res.render("shop/product-detail", {
-//             pageTitle: product.title,
-//             path: "/products",
-//             product: product,
-//           });
-//         } else {
-//           res.redirect("/products");
-//         }
-//       })
-//       .catch((err) => console.log(err));
-//   } else {
-//     res.redirect("/products");
-//   }
-// };
+export const getProduct: IRequestHandler = (req, res, next) => {
+  const prodId = req.params.productId;
+  if (!prodId) {
+    res.redirect("/products");
+  }
+  Product.findById(prodId)
+    .then((product) => {
+      if (product) {
+        res.render("shop/product-detail", {
+          pageTitle: product.title,
+          path: "/products",
+          product: product,
+        });
+      } else {
+        res.redirect("/products");
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
 // export const postOrder: IRequestHandler = (req, res, next) => {
 //   const user = req.user;
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,23 +1,24 @@
 import { Router } from "express";
-import {} from // getCart,
-// getCheckout,
-// getIndex,
-// getOrders,
-// getProduct,
-// getProducts,
-// postCart,
-// postCartDeleteProduct,
-// postOrder,
-"../controllers/shop.ts";
+import {
+  // getCheckout,
+  getIndex,
+  // getOrders,
+  getProduct,
+  getProducts,
+  // postCart,
+  // postCartDeleteProduct,
+  // postOrder,
+  // getCart,
+} from "../controllers/shop.ts";
 
 const router = Router();
 
-// router.get("/", getIndex);
-// router.get("/products", getProducts);
+router.get("/", getIndex);
+router.get("/products", getProducts);
 // router.get("/cart", getCart);
 // router.post("/cart", postCart);
 // router.post("/cart-delete-item", postCartDeleteProduct);
-// router.get("/products/:productId", getProduct);
+router.get("/products/:productId", getProduct);
 // router.post("/create-order", postOrder);
 // router.get("/orders", getOrders);
 // // router.get("/checkout", getCheckout);
